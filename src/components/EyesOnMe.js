1420f3b9a5ae5063a18373d1ab7987e61d5d5341
thomas <thomasmonta98@gmail.com>
2021-07-19 17:13:03 +1000

    done with assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+import React, { Component } from "react";
+
+class EyesOnMe extends Component {
+
+
+    render(){
+        return <button onFocus={() => console.log('Good!')} onBlur={() => console.log('Hey! Eyes on me!')}>click!</button>
+    }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React, { Component } from "react";
+
+class Keypad extends Component {
+
+
+    render(){
+        return (<div>
+            <form>
+            <input type='password' onKeyUp={e => console.log(`Entering password...`)}/>
+            </form>
+            <button>click</button>
+            </div>)
+    }
+}
+
+export default Keypad
